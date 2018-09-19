f008a3d98a195c00288eeb2bc918f9f9915d3653
Richard Cargill <richard.r.cargill@gmail.com>
2018-09-19 12:07:39 +0100

    adding exact path

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Header from '../components/Header.js';
+import Footer from '../components/Footer.js';
 
 const Layout = ({ children, data }) => {
   const title = data.site.siteMetadata.title;
