09e38abda43ff8af2402f8d356d851a3367409bc
Richard Cargill <richard.r.cargill@gmail.com>
2018-09-22 16:27:19 +0100

    working on password component

--- src/components/Password.js
+++ src/components/Password.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import sha1 from 'sha1';
+import { navigateTo } from "gatsby-link";
+import {get} from 'axios';
 
 export default class Password extends React.Component {
   constructor(props) {
@@ -15,15 +17,28 @@ export default class Password extends React.Component {
 
   handleSubmit(event) {
     const hash = sha1(this.state.value);
-    const newLocation = `${hash}${this.props.url}`;
+    const requestUrl = `/annalisa.space/${hash}${this.props.url}`;
+    console.log(requestUrl);
     event.preventDefault();
-    fetch(hash)
+
+
+    // instead let's try updating the requestUrl in state and generating a link
+    // element for it and see if I create a link with the correct href if gatsby
+    // link works
+    console.log('updat 1');
+          // navigateTo(requestUrl);
+    get(requestUrl)
       .then(r => {
-        window.location = newLocation;
-      })
-      .catch(e => {
-        console.log('password incorrect');
-      });
+        console.log(r.data);
+        const is404 = !~r.data.indexOf('NOT FOUND')
+        console.log(is404);
+        if(is404) {
+          console.log('password is invalid');
+        } else {
+          navigateTo(requestUrl);
+          return;
+        }
+      }).catch(err => console.log('password is invalid'));
   }
 
   render() {
