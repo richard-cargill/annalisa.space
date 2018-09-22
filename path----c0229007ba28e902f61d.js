11c6790d36051b72d3a4424d126ec4df2f15d107
Richard Cargill <richard.r.cargill@gmail.com>
2018-09-22 16:23:24 +0100

    Updates

--- path----c0229007ba28e902f61d.js
+++ path----c0229007ba28e902f61d.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{104:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Site logo"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{107:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Site logo"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----c0229007ba28e902f61d.js.map
