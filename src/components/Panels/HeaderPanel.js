384c2fe2b6c90a17c759abb07a971a2fd9f59bf9
Richard Cargill <richard.r.cargill@gmail.com>
2018-10-22 22:54:55 +0100

    adding in some fixes for how header panel is loading images and removing top padding

--- src/components/Panels/HeaderPanel.js
+++ src/components/Panels/HeaderPanel.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HeaderPanel = ({ text, media }) => {
-  const url = !media && !media.file ? 'none' : `no-repeat url(${media.file.url}) center center`
+  const url = media && media.file ? `no-repeat url(${media.file.url}) center center`: 'none'
   return (
     <section
       className="headerPanel"
