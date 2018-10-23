b710b1ca9fb9dd4ddf6227afc419e0239fb46e16
Richard Cargill <richard.r.cargill@gmail.com>
2018-10-24 00:08:58 +0100

    update background colours for paging and aligning to the top of the element

--- src/components/Paging.js
+++ src/components/Paging.js
@@ -8,18 +8,20 @@ const Paging = ({ prev, next }) => {
   const nextTitle = next.title ? next.title : next.name
 
   return (
-    <nav className="paging container" aria-label="Paging">
-      <Link className="paging__item" role="menuitem" to={prev.slug}>
-        <small className="prev-arrow">Previous</small>
-        <h4 className="pageSelector__title">{prevTitle}</h4>
-        <p className="paging__description">{truncateTextAt(prev.description, 100)}</p>
-      </Link>
-      <Link className="paging__item" role="menuitem" to={next.slug}>
-        <small className="next-arrow">Next</small>
-        <h4 className="pageSelector__title">{nextTitle}</h4>
-        <p className="paging__description">{truncateTextAt(next.description, 100)}</p>
-      </Link>
-    </nav>
+    <aside className="background-alt">
+      <nav className="paging container" aria-label="Paging">
+        <Link className="paging__item" role="menuitem" to={prev.slug}>
+          <small className="prev-arrow">Previous</small>
+          <h4 className="pageSelector__title">{prevTitle}</h4>
+          <p className="paging__description">{truncateTextAt(prev.description, 100)}</p>
+        </Link>
+        <Link className="paging__item" role="menuitem" to={next.slug}>
+          <small className="next-arrow">Next</small>
+          <h4 className="pageSelector__title">{nextTitle}</h4>
+          <p className="paging__description">{truncateTextAt(next.description, 100)}</p>
+        </Link>
+      </nav>
+    </aside>
   )
 }
 
